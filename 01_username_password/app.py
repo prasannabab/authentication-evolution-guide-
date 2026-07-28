a508ce050aa32f9ae373030c0863f8583a136ce0
prasanna-babu <33339812+prasannabab@users.noreply.github.com>
2026-07-28 07:09:28 +0530

    Implement basic username and password authentication

--- 01_username_password/app.py
+++ 01_username_password/app.py
@@ -1,1 +1,11 @@
+users = {
+    "alice": "password123"
+}
 
+username = input("Username: ")
+password = input("Password: ")
+
+if users.get(username) == password:
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
