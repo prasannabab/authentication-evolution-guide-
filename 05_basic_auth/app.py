bee899c9140a7df1df1e48ba435b70081c3fd7a8
prasanna-babu <33339812+prasannabab@users.noreply.github.com>
2026-07-29 06:09:24 +0530

    Update app.py

--- 05_basic_auth/app.py
+++ 05_basic_auth/app.py
@@ -1,1 +1,17 @@
+import requests
 
+response = requests.get(
+    "https://httpbin.org/basic-auth/user/pass",
+    auth=("user", "pass")
+)
+
+print(response.status_code)
+
+
+import base64
+
+encoded = "YWxpY2U6cGFzc3dvcmQxMjM="
+
+decoded = base64.b64decode(encoded)
+
+print(decoded.decode())
