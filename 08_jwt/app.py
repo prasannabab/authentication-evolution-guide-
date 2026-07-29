9d6831a2b36d624d0709e3995e41c35f5e60daf9
prasanna-babu <33339812+prasannabab@users.noreply.github.com>
2026-07-29 06:06:03 +0530

    Add JWT encoding and decoding example

--- 08_jwt/app.py
+++ 08_jwt/app.py
@@ -1,1 +1,24 @@
+import jwt
 
+payload = {
+    "user": "Alice",
+    "role": "admin"
+}
+
+token = jwt.encode(
+    payload,
+    "secret",
+    algorithm="HS256"
+)
+
+print(token)
+
+
+# deccode
+decoded = jwt.decode(
+    token,
+    "secret",
+    algorithms=["HS256"]
+)
+
+print(decoded)
